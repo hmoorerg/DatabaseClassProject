30578637b53487d33d1a042fe725c6128a95ee1e
neel2435 <77035089+neel2435@users.noreply.github.com>
2022-06-07 15:57:45 -0700

    edited filepaths

--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,17 +1,17 @@
 COPY MENU
-FROM 'menu.csv'
+FROM '/DatabaseClassProject/data/menu.csv'
 WITH DELIMITER ';';
 
 COPY USERS
-FROM 'users.csv'
+FROM '/DatabaseClassProject/data/users.csv'
 WITH DELIMITER ';';
 
 COPY ORDERS
-FROM 'orders.csv'
+FROM '/DatabaseClassProject/data/orders.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE orders_orderid_seq RESTART 87257;
 
 COPY ITEMSTATUS
-FROM 'itemStatus.csv'
+FROM '/DatabaseClassProject/data/itemStatus.csv'
 WITH DELIMITER ';';
 
